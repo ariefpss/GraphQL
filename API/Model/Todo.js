a241c81aa7485ddfd1dcf14c5fa93a3a8f54f60d
ariefpss <ariefpss>
2020-07-16 10:38:56 +0700

    membuat agar hanya aktivitas user login

--- API/Model/Todo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const todoSchema = mongoose.Schema({
-    id: mongoose.Schema.Types.ObjectId,
-    user_id: mongoose.Schema.Types.ObjectId,
-    task: String
-});
-
-module.exports = mongoose.model('Todo', todoSchema);
