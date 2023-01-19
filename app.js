018750d007e62f671eefe17e9bd0dd005624e405
ariefpss <permanaarief8@gmail.com>
2023-01-19 15:52:58 +0900

    modified:   app.js 	modified:   package.json

--- app.js
+++ app.js
@@ -9,13 +9,18 @@ const chekAuth = require('./API/Middleware/chekauth');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-mongoose.connect(
-    'mongodb://localhost:27017/graphql',
-    {useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true,},
-    () => {
-        console.log("Successfully connect database");   
-    }
-);
+mongoose
+    .connect('mongodb://127.0.0.1:27017/graphql',{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connect to the database!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
 
 app.use(chekAuth);
 
